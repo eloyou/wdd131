6985499f5551e7f6092ab288bd39a71ee09f0655
Angelo Caringal <145478092+eloyou@users.noreply.github.com>
2024-10-04 00:59:29 +0800

    Update place.js

--- scripts/place.js
+++ scripts/place.js
@@ -1,13 +1,12 @@
 
 
-const windchill = document.querySelector("#windchill");
+
 let temp = 26;
 let wind = 5;
 
 function windChill(temp,wind) {
-    
+    const windchill = document.querySelector("#windchill");
     const windchill_compute = 13.12 + (0.6215 * temp) - (11.37 * Math.pow(wind, 0.16)) + (0.3965 * temp * Math.pow(wind, 0.16));
-
     return windchill.textContent = `${windchill_compute.toFixed(2)}°C`;
 }
 
